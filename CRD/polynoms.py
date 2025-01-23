2f4bec13a119c0b609cde0f9fbc9e686ee7d4cfa
Alexander <aspapkov@gmail.com>
2025-01-24 01:06:37 +0300

    Replaced coef name in Polynom class with coefficients. Added typing and style corrections

--- CRD/polynoms.py
+++ CRD/polynoms.py
@@ -2,52 +2,51 @@ from ops import Galois
 from typing import List
 
 
-
 class Polynom:
     def __init__(self, data: List[Galois]):
-        self.coef: List[Galois] = data
+        self.coefficients: List[Galois] = data
 
     def __str__(self):
         line: str = ""
-        for pos, coefficient in enumerate(self.coef):
-            #line += f"{c} "
-            line += f"{hex(coefficient.val)[2:].upper()} "
+        for pos, coef in enumerate(self.coefficients):
+            # line += f"{c} "
+            line += f"{hex(coef.val)[2:].upper()} "
         line += "\n"
         return line
 
     def message(self) -> str:
         message: str = ""
-        for i in self.coef:
+        for i in self.coefficients:
             message += chr(i.val) if i.val != 0 else ""
         return message
 
     def __add__(self, other):
-        l1: int = len(self.coef)
-        l2: int = len(other.coef)
+        l1: int = len(self.coefficients)
+        l2: int = len(other.coefficients)
         if l1 > l2:
-            other.coef = other.coef + [Galois() for i in range(l1 - l2)]
+            other.coefficients = other.coefficients + [Galois() for i in range(l1 - l2)]
         else:
-            self.coef = self.coef + [Galois() for i in range(l2 - l1)]
+            self.coefficients = self.coefficients + [Galois() for i in range(l2 - l1)]
 
         new = []
-        for i, j in zip(self.coef, other.coef):
+        for i, j in zip(self.coefficients, other.coefficients):
             new.append(i + j)
         return Polynom(new)
 
     def __mul__(self, other):
-        l1: int = len(self.coef)
-        l2: int = len(other.coef)
+        l1: int = len(self.coefficients)
+        l2: int = len(other.coefficients)
         new_len: int = l1 + l2 - 1
-        new_coef: List[Galois] = [Galois() for _ in range(new_len)]
-        for i, el1 in enumerate(self.coef):
-            for j, el2 in enumerate(other.coef):
-                new_coef[i + j] += (el1 * el2)
-        return Polynom(new_coef)
+        new_coefficients: List[Galois] = [Galois() for _ in range(new_len)]
+        for i, el1 in enumerate(self.coefficients):
+            for j, el2 in enumerate(other.coefficients):
+                new_coefficients[i + j] += (el1 * el2)
+        return Polynom(new_coefficients)
 
     def discard_zeroes(self):
-        for i in reversed(self.coef):
+        for i in reversed(self.coefficients):
             if not i.val:
-                self.coef.pop()
+                self.coefficients.pop()
             else:
                 break
         return self
@@ -55,14 +54,14 @@ class Polynom:
     def compute(self, n: Galois) -> Galois:
         s = Galois()
         power = Galois(1)
-        for i in self.coef:
+        for i in self.coefficients:
             s += power * i
             power *= n
         return s
 
     def divide_by(self, other: 'Polynom') -> ('Polynom', 'Polynom'):
-        dividend: List[Galois] = self.coef
-        divisor: List[Galois] = other.coef
+        dividend: List[Galois] = self.coefficients
+        divisor: List[Galois] = other.coefficients
 
         quotient = [Galois() for i in range(len(dividend))]
 
@@ -82,19 +81,18 @@ class Polynom:
 
             tmp = Polynom(tmp_coef) * other + Polynom(dividend)
             tmp.discard_zeroes()
-            dividend = tmp.coef
+            dividend = tmp.coefficients
 
     def derivative(self) -> 'Polynom':
-        if len(self.coef) == 1:
+        if len(self.coefficients) == 1:
             return Polynom([Galois()])
         coef: List[Galois] = []
-        for i in range(len(self.coef)):
+        for i in range(len(self.coefficients)):
             if i % 2 == 0:
                 coef.append(Galois())
             else:
-                coef.append(self.coef[i])
+                coef.append(self.coefficients[i])
         return Polynom(coef[1:])
-
 
     @staticmethod
     def make_poly(data: str) -> 'Polynom':
